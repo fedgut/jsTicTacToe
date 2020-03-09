9b23d2beb4723a7b56337a6b62ef07f15aa3b72c
Alisson Peres <aperes.dev@gmail.com>
2020-03-09 13:38:19 -0300

    update script.js file

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,21 @@
 const cells = document.querySelectorAll('.cell-hover');
-const wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
-  [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-
-const Player = (name) => {
-  let wins = 0;
-  const getName = () => name;
-  const getWins = () => wins;
-  const setWin = () => {
-    wins += 1;
-    return wins;
-  };
-  return { getName, getWins, setWin };
-};
-
-const GameBoard = (pl1, pl2) => {
-  player(pl1);
-  player(pl2);
-  const board = [null, null, null, null, null, null, null, null, null];
-  const setMove = (pos, sign) => {
-    board[pos] = sign;
-    return board;
-  };
-  const getBoard = () => board;
-  return { setMove, getBoard };
-};
 
 const setImgSrc = (node, sign) => {
   node.src = `./assets/${sign}-img.png`;
 };
 
-const imgSrc = './assets/x-img.png';
 let sign = 'x';
-cells.forEach((item) => {
-  item.onclick = function () {
-    const childElement = this.id;
-    newGame.setMove(childElement, sign);
-    console.log(newGame.getBoard());
-    // board[childElement] = sign;
-    setImgSrc(this.firstElementChild, sign);
-    sign = (sign === 'x' ? 'o' : 'x');
+cells.forEach(item => {
+  item.onclick = function changeImg() {
+    if (this.firstElementChild.src.includes('no-img.png')) {
+      this.classList.remove('cell-hover');
+      setImgSrc(this.firstElementChild, sign);
+      sign = sign === 'x' ? 'o' : 'x';
+    }
   };
 });
 
-const startGame = (form) => {
+const startGame = form => {
   const initMessage = document.getElementById('initial-message');
   const startMessage = document.getElementById('start-game');
   if (form[0].value === '' || form[1].value === '') {
@@ -51,12 +24,10 @@ const startGame = (form) => {
   } else {
     initMessage.classList.add('d-none');
     startMessage.classList.remove('d-none');
-    // const newGame = GameBoard(form[0].value, form[1].value);
-    // return newGame;
   }
 };
 
-function ready() {
+window.onload = function ready() {
   const playerForm = document.getElementById('player-form');
   playerForm.reset();
   const gameBoard = document.getElementById('game-board');
@@ -64,36 +35,15 @@ function ready() {
   const btn = document.getElementById('submit');
   const initMessage = document.getElementById('initial-message');
   const startMessage = document.getElementById('start-game');
-  const select = document.querySelectorAll('#player-input');
+  const select = document.querySelectorAll('.player-input');
   select.forEach((item, i) => {
     const ipt = item.getElementsByTagName('input');
-    ipt[0].addEventListener('input', (event) => {
+    ipt[0].addEventListener('input', event => {
       if (playerForm.name1.value > '' && playerForm.name2.value > '') {
         btn.disabled = false;
-        // initMessage.classList.add('d-none');
-        // startMessage.classList.remove('d-none');
       } else {
         btn.disabled = true;
-        // startMessage.classList.add('d-none');
-        // initMessage.classList.remove('d-none');
       }
-      // console.log(event.srcElement.value);
     });
   });
-
-  // rows.forEach(elem => elem.classList.toggle('d-none'));
-  // console.log(gameBoard);
-  // console.log(rows);
-}
-
-if (document.readyState !== 'loading') {
-  ready();
-} else {
-  document.addEventListener('DOMContentLoaded', ready);
-}
-
-
-// pl2.addEventListener('input', (eve) => {
-//   console.log(eve.srcElement.value)
-// })
-// btn.disabled = false
+};
